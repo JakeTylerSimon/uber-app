e19396dd019942ecee025cc5aeb1b02e664f41e6
JakeTylerSimon <jtsimonbaseball@gmail.com>
2022-08-25 06:19:26 -0700

    commit

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,10 @@
 import { Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native'
+import { selectOrigin } from '../slices/navSlice';
+import { useSelector } from 'react-redux';
 
 const data = [
     {
@@ -18,20 +22,23 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
+
   return (
     <FlatList
         data={data}
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({item}) => (
-            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                <View>
+            <TouchableOpacity disabled={!origin} style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} onPress={() => navigation.navigate(item.screen)} >
+                <View style={tw`${!origin && 'opacity-20'}`}>
                     <Image
                         style={{  width: 120, height: 120, resizeMode: "contain" }}
                         source={{ uri: item.image }}
                     />
                     <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                    
+                    <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color= 'white' type='antdesign' />
                 </View>
             </TouchableOpacity>
         )}
